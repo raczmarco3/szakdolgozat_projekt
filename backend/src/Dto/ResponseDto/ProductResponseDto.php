6a4b67aa70bb5b191dc73d04c0aecd27d7a46808
Marco <raczmarco3@gmail.com>
2023-04-06 15:45:29 +0200

    -ProductResponseDto megirasa -ProductService megirasa -CategoryService: findBy lecserelese findOneBy-ra -Product: user ideiglenesen kikommentezve

---
+++ backend/src/Dto/ResponseDto/ProductResponseDto.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Dto\ResponseDto;
+
+class ProductResponseDto
+{
+    private int $id;
+    private string $name;
+    private int $price;
+    private string $category;
+    private ?\DateTimeImmutable $createdAt;
+    private ?\DateTimeImmutable $updatedAt;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param int $price
+     */
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return CategoryResponseDto
+     */
+    public function getCategory(): CategoryResponseDto
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param CategoryResponseDto $category
+     */
+    public function setCategory(CategoryResponseDto $category): void
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable|null $createdAt
+     */
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTimeImmutable|null
+     */
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable|null $updatedAt
+     */
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}
---
+++ backend/src/Service/ProductService.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Service;
+
+use App\Converter\JsonConverter;
+use App\Dto\RequestDto\ProductRequestDto;
+use App\Dto\ResponseDto\CategoryResponseDto;
+use App\Dto\ResponseDto\ProductResponseDto;
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProductService
+{
+    public function addNewProduct(ProductRepository $productRepository, CategoryRepository $categoryRepository, ProductRequestDto $productRequestDto): JsonResponse
+    {
+        $product = $productRepository->findOneBy(["name" => $productRequestDto->getName()]);
+
+        if($product) {
+            return new JsonResponse(["msg" => "This product already exists!"], 403);
+        }
+
+        $product = new Product();
+        $product = $this->setProduct($product, $productRequestDto, $categoryRepository);
+
+        $category = $categoryRepository->find($productRequestDto->getCategoryId());
+        if(!$category) {
+            return new JsonResponse(["msg" => "Category not found!"], 404);
+        }
+        $product->setCategory($category);
+
+        $productRepository->save($product, true);
+        return new JsonResponse(["msg" => "Product created!"], 201);
+    }
+
+    public function getProducts(SerializerInterface $serializer, ProductRepository $productRepository): JsonResponse
+    {
+        $products = $productRepository->findAll();
+
+        if(empty($products)) {
+            return new JsonResponse(["msg" => "There are no products yet!"], 404);
+        }
+
+        $productResponseDtoArray = [];
+
+        foreach($products as $product)
+        {
+            if($product->getDeleted() == 0) {
+                $productResponseDto = new ProductResponseDto();
+                $productResponseDto->setId($product->getId());
+                $productResponseDto->setName($product->getName());
+                $productResponseDto->setPrice($product->getPrice());
+                $productResponseDto->setCategory($product->getCategory()->getName());
+                $productResponseDto->setCreatedAt($product->getCreatedAt());
+                $productResponseDto->setUpdatedAt($product->getUpdatedAt());
+
+                $productResponseDtoArray[] = $productResponseDto;
+            }
+        }
+        return JsonConverter::jsonResponseConverter($serializer, $productResponseDtoArray);
+    }
+
+    public function deleteProduct($id, ProductRepository $productRepository): JsonResponse
+    {
+        $product = $productRepository->find($id);
+
+        if(!$product) {
+            return new JsonResponse(["msg" => "Product not found!"], 404);
+        }
+
+        $productRepository->remove($product, true);
+        return new JsonResponse(["msg" => "Product deleted!"], 200);
+    }
+
+    public function editProduct($id, ProductRepository $productRepository, CategoryRepository $categoryRepository,
+                                ProductRequestDto $productRequestDto, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $product = $productRepository->find($id);
+
+        if(!$product) {
+            return new JsonResponse(["msg" => "Product not found!"], 404);
+        }
+
+        if($product->getName() != $productRequestDto->getName() && $productRepository->findOneBy(["name" => $productRequestDto->getName()])) {
+            return new JsonResponse(["msg" => "This product name already exists!"], 403);
+        }
+
+        $product = $this->setProduct($product, $productRequestDto);
+        $category = $categoryRepository->find($productRequestDto->getCategoryId());
+        if(!$category) {
+            return new JsonResponse(["msg" => "Category not found!"], 404);
+        }
+        $product->setCategory($category);
+
+        $entityManager->flush();
+        return new JsonResponse(["msg" => "Product updated!"], 200);
+    }
+
+    private function setProduct(Product $product, ProductRequestDto $productRequestDto): Product
+    {
+        $product->setName($productRequestDto->getName());
+        $product->setPrice($productRequestDto->getPrice());
+        $product->setDeleted(0);
+
+        $date = new DateTimeImmutable("now");
+        $product->setUpdatedAt($date);
+
+        return $product;
+    }
+}
