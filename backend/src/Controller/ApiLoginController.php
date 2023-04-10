b9d9c11c4b4eb4c8a68668db4a72b0e019b76581
Marco <raczmarco3@gmail.com>
2023-04-10 18:04:33 +0200

    -ApiLoginController megirasa

---
+++ backend/src/Controller/ApiLoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class ApiLoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login')]
+    public function index(#[CurrentUser] ?User $user): JsonResponse
+    {
+        if (null === $user) {
+            return new JsonResponse(["msg" => "Missing credentials!"], 401);
+        }
+
+        return new JsonResponse(["msg" => "Bejelentkezés sikeres!"], 200);
+    }
+}
