53ab9c1e8107c508dca9d789cc00f6cd4454b66f
Marco <raczmarco3@gmail.com>
2023-04-15 10:41:28 +0200

    frontend: status szerkesztese

--- frontend/src/app/admin/status/edit-status/edit-status.component.ts
+++ frontend/src/app/admin/status/edit-status/edit-status.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {Method} from "../../admin-model/method";
+import {FormControl, FormGroup} from "@angular/forms";
+import {StatusService} from "../../admin-service/status-service";
 
 @Component({
   selector: 'app-edit-status',
@@ -6,5 +10,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-status.component.css']
 })
 export class EditStatusComponent {
+  jsonContent: JSON;
+  msg: string;
+  obj: any;
+  constructor(public dialogRef: MatDialogRef<EditStatusComponent>, private statusService: StatusService,
+              @Inject(MAT_DIALOG_DATA) public data: Method) {}
 
+  editStatusForm = new FormGroup({
+    name: new FormControl(),
+  });
+  onClose() {
+    this.dialogRef.close();
+  }
+  onSubmit(event: any) {
+    if(event.submitter.name == "add") {
+      this.obj = {
+        "id": this.data.id,
+        "name": this.editStatusForm.get('name')?.value,
+      };
+
+      this.jsonContent = <JSON>this.obj;
+
+      this.statusService.editStatus(this.data.id, this.jsonContent).subscribe(
+        {
+          next: (response) => {
+            this.dialogRef.close();
+          },
+          error: (msg) => {
+            this.msg = msg.error.msg;
+          }
+        }
+      );
+    }
+  }
 }
