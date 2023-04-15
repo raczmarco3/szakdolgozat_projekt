dbbc2e5da4f930416b7b264d63a08f6f6b1da006
Marco <raczmarco3@gmail.com>
2023-04-15 20:23:16 +0200

    frontend: mainpage termekek megjelenitesenek javitasa

--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -13,6 +13,7 @@ export class MainPageComponent {
   errorStatus: number;
   pagination: number = 1;
   allProducts: number;
+  public arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   constructor(private mainPageService: MainPageService) { }
 
