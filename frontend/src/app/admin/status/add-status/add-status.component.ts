41eff0fba1150a255cf28a58bbc807fee90e7f56
Marco <raczmarco3@gmail.com>
2023-04-15 10:37:46 +0200

    frontend: status hozzaadas

--- frontend/src/app/admin/status/add-status/add-status.component.ts
+++ frontend/src/app/admin/status/add-status/add-status.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
+import {FormControl, FormGroup} from "@angular/forms";
+import {StatusService} from "../../admin-service/status-service";
 
 @Component({
   selector: 'app-add-status',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-status.component.css']
 })
 export class AddStatusComponent {
+  jsonContent: JSON;
+  msg: string;
+  obj: any;
 
+  constructor(public dialogRef: MatDialogRef<AddStatusComponent>, private statusService: StatusService) {}
+
+  addStatusForm = new FormGroup({
+    name: new FormControl(),
+  });
+  onClose() {
+    this.dialogRef.close();
+  }
+  onSubmit(event: any) {
+    if(event.submitter.name == "add") {
+      this.obj = {
+        "name": this.addStatusForm.get('name')?.value,
+      };
+
+      this.jsonContent = <JSON>this.obj;
+
+      this.statusService.addStatus(this.jsonContent).subscribe(
+        {
+          next: (response) => {
+            this.dialogRef.close();
+          },
+          error: (msg) => {
+            this.msg = msg.error.msg;
+          }
+        }
+      );
+    }
+  }
 }
